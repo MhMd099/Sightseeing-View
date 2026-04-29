cf9324e20d03fc6b20385b9e9c968aa52aba2fe8
MhMd099 <moalalhussein@gmail.com>
2026-04-29 10:39:09 +0200

    feat: init SightseeingDetail component mit props

---
+++ sightseeing-view/src/components/SightseeingDetail.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="place" class="detail-card">
+    <h2>{{ place.name }}</h2> <p><strong>Adresse:</strong> {{ place.address }}</p>
+    <p><strong>Öffnungszeiten:</strong> {{ place.opening_hours || 'Nicht verfügbar' }}</p>
+    <a :href="place.website" target="_blank">Website besuchen</a> </div>
+</template>
+
+<script>
+export default {
+  props: ['place'] // Hier kommen die Daten von Lukas an 
+}
+</script>
